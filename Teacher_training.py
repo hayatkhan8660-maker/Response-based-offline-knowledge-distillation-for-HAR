4cc3ea50b051495ca2ac95bab73cb4efc85a87c5
Hayat Ullah <36618567+hayatkhan8660-maker@users.noreply.github.com>
2023-10-19 18:22:27 -0500

    Teacher_training.py

--- Teacher_training.py
+++ Teacher_training.py
@@ -20,45 +20,52 @@ import os
 import datetime
 from distiller import Distiller
 from keras.layers.core import Dense, Dropout, Flatten
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data', type=str, required=True, help="path to frames dataset")
+parser.add_argument('--annotations', type=str, required=True, help="path to labels")
+parser.add_argument('--batch_size', type=int, required=True, help="Batch size")
+parser.add_argument('--epochs', type=int, required=True, help="Number of epochs")
+parser.add_argument('--output_path', type=str, required=True, help="path for saving trained model")
+parser.add_argument('--log_path', type=str, required=True, help="path for saving trained history")
+parser.add_argument('--task', type=str, required=True, help="finetuning pre-trained C3D -> use [fintuning_C3D_Sports1M] or train C3D locally -> use [train_local_teacher]")
+args = parser.parse_args()
 
 
-
-dataset_frames = np.load('./UCF101/features.npy')
-dataset_labels = np.load('./UCF101/labels.npy')
+dataset_frames = np.load(args.data)
+dataset_labels = np.load(args.annotations)
 
 # Split features and labels into training and testing data with 80% training and 20% testing data
 train_x, test_x, train_y, test_y = train_test_split(dataset_frames, dataset_labels, test_size = 0.20, shuffle = True)
 
-
-print("Training Data : ", train_x.shape)
-print("Validation Data : ", test_x.shape)
-
-
-batch_size = 8
-Epochs = 50
+batch_size = args.batch_size
+Epochs = args.epochs
 training_generator = TrainGenerator(train_x, train_y, batch_size)
 val_generator = TrainGenerator(test_x, test_y, batch_size)
 
+nb_classes = len(np.unique(dataset_labels, return_counts=True)[0])
 
-nb_classes = 101
+if args.task == "fintuning_C3D_Sports1M":
+  teacher = networks.C3D(weights='sports1M')
 
-source_model = networks.C3D(weights='sports1M')
-source_model.summary()
+  # freeze the first 19 layers 
+  for layer in teacher.layers[:19]:
+    layer.trainable = False
 
-# freeze the first 19 layers 
-for layer in source_model.layers[:19]:
-  layer.trainable = False
+  for i, layer in enumerate(teacher.layers):
+    print(i, layer.name, "-", layer.trainable)
 
-for i, layer in enumerate(source_model.layers):
-  print(i, layer.name, "-", layer.trainable)
+  print(teacher.layers[-2].output)
 
-print(source_model.layers[-2].output)
-
-X = source_model.layers[-2].output
-predictions = Dense(nb_classes, activation='softmax')(X)
-teacher = Model(source_model.input, predictions)
-teacher.summary()
-
+  X = teacher.layers[-2].output
+  predictions = Dense(nb_classes, activation='softmax')(X)
+  teacher = Model(teacher.input, predictions)
+  teacher.summary()
+elif args.task == "train_local_teacher":
+  teacher = networks.Teacher_C3D(nb_classes)
+else:
+  print("Task is not Specified.....")
 
 opt = tf.keras.optimizers.Adam(learning_rate=0.0001)
 # Compile the model and specify loss function, optimizer and metrics values to the model
@@ -75,8 +82,8 @@ model_training_history = teacher.fit(
 )
 test_history = teacher.evaluate(val_generator)
 
-teacher.save('./Fintuned_teacher_with_pretrained_C3D_Sports1M_weights.h5')
-np.save('./Fintuned_teacher_with_pretrained_C3D_Sports1M_training_history',model_training_history.history)
+teacher.save(args.output_path)
+np.save(args.log_path,model_training_history.history)
 
 
 
